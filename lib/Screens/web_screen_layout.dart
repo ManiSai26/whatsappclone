8542c947b7f2e0d57f1e46f29f7d141e8c3517c8
ManiSai26 <kamutalamanisai26@gmail.com>
2022-11-19 19:09:13 +0530

    WebLayout

--- lib/Screens/web_screen_layout.dart
+++ lib/Screens/web_screen_layout.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:whatsappclone/Widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const  Scaffold(
-      body: Center(
-        child: Text("This is a Web Screen Layout"),
+    return   Scaffold(
+      body:
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children:  [
+                  ContactsList(),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width*0.75,
+            decoration: const BoxDecoration(
+              image: DecorationImage(image: 
+              AssetImage('assets/backgroundImage.png'),
+              fit: BoxFit.cover,),
+            ),
+
+          ),
+        ],
       ),
     );
   }
